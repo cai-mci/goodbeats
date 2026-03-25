69e0d6cc057de3d68098470ce476e1af928b56f2
Kiana Shen <kiana.shen22@tamu.edu>
2026-03-25 18:06:20 -0500

    created functions

--- data_extraction.py
+++ data_extraction.py
@@ -5,6 +5,10 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import sys
 
+# MP3 downloads
+import yt_dlp as youtube_dl
+from __future__ import unicode_literals
+
 load_dotenv()
 
 api_url = os.getenv("SPOTIFY_CLIENT_ID")
@@ -17,5 +21,8 @@ def get_music_features(track_url):
     track_id = track_url.split(':')[-1].split('/')[-1].split('?')[0]
 
     audio_features = sp.audio_features([track_id])[0]
+
+
+# TODO: create function to extract features from mp3 file
 
 print("hi")
---
+++ download_audio_files.py
@@ -0,0 +1,8 @@
+
+
+# TODO: For MODEL --> Convert links from top_50s_chart.csv to mp3 files 
+
+# TODO: create function to convert url to mp3 file  (kiana)
+
+
+
