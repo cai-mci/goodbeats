bb9298732ebbd6585e478ead6324ef6ed1348ed5
Satyaki Poddar <satyakipoddar202@gmail.com>
2026-04-07 19:50:08 -0500

    add librosa feature extraction

--- data_extraction.py
+++ data_extraction.py
@@ -1,19 +1,50 @@
 import os
-import requests
-from dotenv import load_dotenv
 import spotipy
+from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
-import sys
+import librosa
+import yt_dlp
+import numpy as np
 
 load_dotenv()
 
-api_url = os.getenv("SPOTIFY_CLIENT_ID")
-base_url = os.getenv("SPOTIFY_REDIRECT_URI")
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-if not api_url or not base_url:
-    raise RuntimeError("Missing API_URL or BASE_URL check .env file")
+if not client_id or not client_secret:
+    raise RuntimeError("Missing SPOTIFY_CLIENT_ID or SPOTIFY_CLIENT_SECRET in .env file")
 
-def get_music_features(track_url):
-    track_id = track_url.split(':')[-1].split('/')[-1].split('?')[0]
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id=client_id,
+    client_secret=client_secret
+))
 
-    audio_features = sp.audio_features([track_id])[0]
+def extract_features(youtube_url):
+
+    os.makedirs('audio', exist_ok=True)
+
+    ydl_opts = {
+        'format' : 'bestaudio/best',
+        'outtmpl' : 'audio/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(youtube_url, download= True)
+        filename = f"audio/{info['title']}.mp3"
+    
+    y, sr = librosa.load(filename)
+    mfccs = librosa.feature.mfcc(y=y, sr=sr).mean(axis=1)
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr).mean(axis=1)
+    tempo, _ =librosa.beat.beat_track(y=y, sr=sr)
+    spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr).mean()
+    zcr = librosa.feature.zero_crossing_rate(y).mean()
+
+    return{
+        'mfccs': mfccs, 'chroma' : chroma, 'tempo' : tempo, 'spectral_centroid' : spectral_centroid, 'zcr' : zcr
+    }
+    
+    
