0e4e47f904eea1abccb2b89ccb7b71d4ba0af045
char-cyber <charu7465@tamu.edu>
2026-04-16 16:32:00 -0500

    added some routing for supabase backend

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,11 @@ import random
 import pandas as pd
 import os
 from dotenv import load_dotenv
-from supabase import create_client, client
+from supabase import create_client, Client
 load_dotenv()
 supabase_url = os.getenv('NEXT_PUBLIC_SUPABASE_URL')
 supabase_key = os.getenv('NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY')
-supabase = create_client(supabase_url, supabase_key)
+supabase: Client = create_client(supabase_url, supabase_key)
 app = Flask(__name__)
 CORS(app) 
 
@@ -22,7 +22,23 @@ def randnums(numsongs):
 # TODO
 @app.route('/api/recommend', methods=['GET'])
 def recommend():
-    return jsonify({"message": "not defined yet"})
+    song = request.args.get("song")
+    if not song:
+        return jsonify({"error": "No song provided"}), 400
+    response = supabase.table('Clean_Features').select("*").execute()
+    songIndex = None
+    #code right now is getting the song index
+    audioMatrix =response.data.to_numpy()
+    for i, row in enumerate(audioMatrix):
+        if row[3].lower() == song.lower():
+            songIndex = i
+            break
+    if songIndex is None:
+        return jsonify({"error": "Song not found"}), 404
+
+    #indexes = top_n_songs(songIndex, 10)
+    recommendations = [audioMatrix.iloc[i]["n_a_m_e"] for i in range(10)]
+    return jsonify({"message": recommendations})
 
 # TODO
 # data = pd.read_csv('Spotify_Song_Attributes.csv')
@@ -31,15 +47,16 @@ def recommend():
 # audioMatrix = data.drop(columns = columns_to_drop)
 # audioFeaturesWithName = audioMatrix.to_numpy()
 
-idDict = {}
-#connecting to supabase to get data
-response = supabase.table('Clean_Features').select('*').execute()
-print(response)
-data = jsonify(response.data)
-print(data)
-for i in range(0, len(data)):
-    idDict[i] = data[i][3]
-print(idDict)
+# idDict = {}
+# #connecting to supabase to get data
+# response = supabase.table('Clean_Features').select('*').execute()
+# print(response)
+# data = jsonify(response.data)
+
+# print(data)
+# for i in range(0, len(data)):
+#     idDict[i] = data[i][3]
+# print(idDict)
 @app.route('/api/random-songs', methods=['GET'])
 def random_songs():
     global idDict
