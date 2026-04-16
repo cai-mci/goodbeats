cdd7dbfbdffe1955db05665224e1b7579d63e50e
char-cyber <charu7465@tamu.edu>
2026-04-15 19:30:42 -0500

    supabase connection

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,18 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
 import pandas as pd
-
+import os
+from dotenv import load_dotenv
+from supabase import create_client, client
+load_dotenv()
+supabase_url = os.getenv('NEXT_PUBLIC_SUPABASE_URL')
+supabase_key = os.getenv('NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY')
+supabase = create_client(supabase_url, supabase_key)
 app = Flask(__name__)
 CORS(app) 
 
 import random
-
+num_songs = 150
 def randnums(numsongs):
     a = random.randint(0, numsongs - 1)
     b = random.randint(0, numsongs - 1)
@@ -19,17 +25,21 @@ def recommend():
     return jsonify({"message": "not defined yet"})
 
 # TODO
-data = pd.read_csv('Spotify_Song_Attributes.csv')
-columns_to_drop = ['artistName', 'msPlayed', 'type', 'id', 
-             'uri', 'track_href', 'analysis_url', 'genre']
-audioMatrix = data.drop(columns = columns_to_drop)
-audioFeaturesWithName = audioMatrix.to_numpy()
+# data = pd.read_csv('Spotify_Song_Attributes.csv')
+# columns_to_drop = ['artistName', 'msPlayed', 'type', 'id', 
+#              'uri', 'track_href', 'analysis_url', 'genre']
+# audioMatrix = data.drop(columns = columns_to_drop)
+# audioFeaturesWithName = audioMatrix.to_numpy()
 
 idDict = {}
-
-for i in range(0, len(audioFeaturesWithName) - 1):
-    idDict[i] = audioFeaturesWithName[i][0]
-
+#connecting to supabase to get data
+response = supabase.table('Clean_Features').select('*').execute()
+print(response)
+data = jsonify(response.data)
+print(data)
+for i in range(0, len(data)):
+    idDict[i] = data[i][3]
+print(idDict)
 @app.route('/api/random-songs', methods=['GET'])
 def random_songs():
     global idDict
